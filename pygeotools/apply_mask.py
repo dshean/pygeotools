05d56c54d9d94c9a9000a473626dcf479069596c
dlilien <skinonframe@gmail.com>
2016-11-08 15:26:03 -0800

    some tweaks to the executables, made everything functions so you can put as console scripts, but then didnt because it looks like writing to usr/local is unwanted

--- pygeotools/apply_mask.py
+++ pygeotools/apply_mask.py
@@ -11,42 +11,46 @@
 import sys, os
 
 import numpy as np
-from osgeo import gdal
 
-from .lib import iolib
-from .lib import warplib
+from pygeotools.lib import iolib
+from pygeotools.lib import warplib
 
-if len(sys.argv) != 3:
-    sys.exit("Usage: %s raster.tif mask.tif" % os.path.basename(sys.argv[0]))
+def main():
+    if len(sys.argv) != 3:
+        sys.exit("Usage: %s raster.tif mask.tif" % os.path.basename(sys.argv[0]))
 
-dem_fn = sys.argv[1]
-mask_fn = sys.argv[2]
+    dem_fn = sys.argv[1]
+    mask_fn = sys.argv[2]
 
-dem_ds, mask_ds = warplib.memwarp_multi_fn([dem_fn, mask_fn], res='first', extent='first', t_srs='first')
+    dem_ds, mask_ds = warplib.memwarp_multi_fn([dem_fn, mask_fn], res='first', extent='first', t_srs='first')
 
-dem_ma_full = iolib.ds_getma(dem_ds)
-mask_ma_full = iolib.ds_getma(mask_ds)
+    dem_ma_full = iolib.ds_getma(dem_ds)
+    mask_ma_full = iolib.ds_getma(mask_ds)
 
-if mask_ma_full.std() != 0:
-    #Input mask filename is actually a DEM, or other masked array
-    #Extract mask
-    mask = np.ma.getmaskarray(mask_ma_full)
-else:
-    #Input mask filename is truly a mask, use directly
-    #If input mask values are zero, valid values are nonzero
-    #Bool True == 1, so need to invert
-    if mask_ma_full.fill_value == 0:
-        mask = ~((mask_ma_full.data).astype(bool))
+    if mask_ma_full.std() != 0:
+        #Input mask filename is actually a DEM, or other masked array
+        #Extract mask
+        mask = np.ma.getmaskarray(mask_ma_full)
     else:
-        mask = (mask_ma_full.data).astype(bool)
+        #Input mask filename is truly a mask, use directly
+        #If input mask values are zero, valid values are nonzero
+        #Bool True == 1, so need to invert
+        if mask_ma_full.fill_value == 0:
+            mask = ~((mask_ma_full.data).astype(bool))
+        else:
+            mask = (mask_ma_full.data).astype(bool)
 
-#Add dilation step for buffer
+    #Add dilation step for buffer
 
-#newmask = np.logical_or(np.ma.getmaskarray(dem_ma_full), mask)
+    #newmask = np.logical_or(np.ma.getmaskarray(dem_ma_full), mask)
 
-print("Creating new array with updated mask")
-dem_ma_full = np.ma.array(dem_ma_full, mask=mask)
+    print("Creating new array with updated mask")
+    dem_ma_full = np.ma.array(dem_ma_full, mask=mask)
 
-print("Writing out masked full-res DEM")
-dem_fn_masked = os.path.splitext(dem_fn)[0]+'_masked.tif'
-iolib.writeGTiff(dem_ma_full, dem_fn_masked, dem_ds, create=True, sparse=True) 
+    print("Writing out masked full-res DEM")
+    dem_fn_masked = os.path.splitext(dem_fn)[0]+'_masked.tif'
+    iolib.writeGTiff(dem_ma_full, dem_fn_masked, dem_ds, create=True, sparse=True)
+
+
+if __name__ == '__main__':
+    main()
--- pygeotools/ndvtrim.py
+++ pygeotools/ndvtrim.py
@@ -8,43 +8,49 @@
 import sys
 import os
 
-from .lib import iolib
-from .lib import malib
+from pygeotools.lib import iolib
+from pygeotools.lib import malib
 
-if len(sys.argv) != 2:
-    sys.exit('Usage is: %s raster.tif' % os.path.basename(sys.argv[0]))
 
-src_fn = sys.argv[1]
-#This is a wrapper around gdal.Open()
-src_ds = iolib.fn_getds(src_fn)
-src_gt = src_ds.GetGeoTransform()
-src_dt = src_ds.GetRasterBand(1).DataType
-src_srs = src_ds.GetProjection()
+def main():
+    if len(sys.argv) != 2:
+        sys.exit('Usage is: %s raster.tif' % os.path.basename(sys.argv[0]))
 
-print("Loading input raster into masked array")
-bma = iolib.ds_getma(src_ds)
+    src_fn = sys.argv[1]
+    #This is a wrapper around gdal.Open()
+    src_ds = iolib.fn_getds(src_fn)
+    src_gt = src_ds.GetGeoTransform()
+    src_dt = src_ds.GetRasterBand(1).DataType
+    src_srs = src_ds.GetProjection()
 
-print("Computing min/max indices for mask")
-edge_env = malib.edgefind2(bma)
+    print("Loading input raster into masked array")
+    bma = iolib.ds_getma(src_ds)
 
-print("Updating output geotransform")
-out_gt = list(src_gt)
-#This should be OK, as edge_env values are integer multiples, and the initial gt values are upper left pixel corner
-#Update UL_X
-out_gt[0] = src_gt[0] + src_gt[1]*edge_env[2]
-#Update UL_Y, note src_gt[5] is negative
-out_gt[3] = src_gt[3] + src_gt[5]*edge_env[0]
-out_gt = tuple(out_gt)
+    print("Computing min/max indices for mask")
+    edge_env = malib.edgefind2(bma)
 
-#debug
-#print [0, bma.shape[0], 0, bma.shape[1]]
-#print edge_env
-#print src_gt
-#print out_gt
+    print("Updating output geotransform")
+    out_gt = list(src_gt)
+    #This should be OK, as edge_env values are integer multiples, and the initial gt values are upper left pixel corner
+    #Update UL_X
+    out_gt[0] = src_gt[0] + src_gt[1]*edge_env[2]
+    #Update UL_Y, note src_gt[5] is negative
+    out_gt[3] = src_gt[3] + src_gt[5]*edge_env[0]
+    out_gt = tuple(out_gt)
 
-out_fn = os.path.splitext(src_fn)[0]+'_trim.tif'
-print("Writing out: %s" % out_fn)
-#Extract valid subsection from input array
-#indices+1 are necessary to include valid row/col on right and bottom edges
-iolib.writeGTiff(bma[edge_env[0]:edge_env[1]+1, edge_env[2]:edge_env[3]+1], out_fn, src_ds, gt=out_gt)
-bma = None
+    #debug
+    #print [0, bma.shape[0], 0, bma.shape[1]]
+    #print edge_env
+    #print src_gt
+    #print out_gt
+
+    out_fn = os.path.splitext(src_fn)[0]+'_trim.tif'
+    print("Writing out: %s" % out_fn)
+    #Extract valid subsection from input array
+    #indices+1 are necessary to include valid row/col on right and bottom edges
+    iolib.writeGTiff(bma[edge_env[0]:edge_env[1]+1, edge_env[2]:edge_env[3]+1], out_fn, src_ds, gt=out_gt)
+    bma = None
+
+
+if __name__ == '__main__':
+    main()
