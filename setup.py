6cd2d65794646511bab6e43f852a6d189d8055ef
David Shean <dshean@gmail.com>
2016-11-07 16:34:01 -0800

    initial repo setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pygeotools',
+    version='0.1.0',
+    description='Libraries and command-line utilities for geospatial data processing/analysis',
+    author='David Shean',
+    author_email='dshean@gmail.com',
+    license='MIT',
+    url='https://github.com/dshean/pygeotools',
+    packages=['pygeotools', 'pygeotools.lib'],
+    long_description=open('README.md').read(),
+    install_requires=['numpy','gdal'],
+)
+
