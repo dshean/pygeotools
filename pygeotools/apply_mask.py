dc01e46b2e2dd66db4671e9921324ac159dfdf92
David Shean <dshean@gmail.com>
2017-01-04 20:43:36 -0800

    apply_mask: add argparse, add extent option

--- pygeotools/apply_mask.py
+++ pygeotools/apply_mask.py
@@ -9,6 +9,7 @@
 #True (1) for masked, False (0) for valid - consistent with np.ma
 
 import sys, os
+import argparse
 
 import numpy as np
 
@@ -16,14 +17,30 @@ from pygeotools.lib import iolib
 from pygeotools.lib import warplib
 
 def main():
-    if len(sys.argv) != 3:
-        sys.exit("Usage: %s raster.tif mask.tif" % os.path.basename(sys.argv[0]))
+    parser = argparse.ArgumentParser(description="Apply existing mask to input raster")
+    #Should add support for similar arguments as in warplib - arbitrary extent, res, etc
+    parser.add_argument('-extent', type=str, default='raster', choices=['raster','mask','intersection','union'], \
+                        help='Desired output extent')
+    parser.add_argument('src_fn', type=str, help='Input raster filename')
+    parser.add_argument('mask_fn', type=str, help='Input mask filename (can be existing raster with ndv, or binary mask)')
+    args = parser.parse_args()
 
-    src_fn = sys.argv[1]
-    mask_fn = sys.argv[2]
+    src_fn = args.src_fn
+    if not iolib.fn_check(src_fn):
+        sys.exit("Unable to find src_fn: %s" % src_fn)
 
-    src_ds, mask_ds = warplib.memwarp_multi_fn([src_fn, mask_fn], res='first', extent='first', t_srs='first')
-    #src_ds, mask_ds = warplib.memwarp_multi_fn([src_fn, mask_fn], res='first', extent='intersection', t_srs='first')
+    mask_fn = args.mask_fn
+    if not iolib.fn_check(mask_fn):
+        sys.exit("Unable to find mask_fn: %s" % mask_fn)
+
+    #Determine output extent, default is input raster extent 
+    extent = args.extent 
+    if extent == 'raster':
+        extent = src_fn
+    elif extent == 'mask':
+        extent = mask_fn
+
+    src_ds, mask_ds = warplib.memwarp_multi_fn([src_fn, mask_fn], res='first', extent=extent, t_srs='first')
 
     src_ma_full = iolib.ds_getma(src_ds)
     mask_ma_full = iolib.ds_getma(mask_ds)
