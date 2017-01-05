6584c06e3b15f2df0c51e105c0c894431fed9fd8
David Shean <dshean@gmail.com>
2017-01-04 19:37:01 -0800

    apply_mask: update variable names, comments

--- pygeotools/apply_mask.py
+++ pygeotools/apply_mask.py
@@ -19,20 +19,21 @@ def main():
     if len(sys.argv) != 3:
         sys.exit("Usage: %s raster.tif mask.tif" % os.path.basename(sys.argv[0]))
 
-    dem_fn = sys.argv[1]
+    src_fn = sys.argv[1]
     mask_fn = sys.argv[2]
 
-    dem_ds, mask_ds = warplib.memwarp_multi_fn([dem_fn, mask_fn], res='first', extent='first', t_srs='first')
+    src_ds, mask_ds = warplib.memwarp_multi_fn([src_fn, mask_fn], res='first', extent='first', t_srs='first')
+    #src_ds, mask_ds = warplib.memwarp_multi_fn([src_fn, mask_fn], res='first', extent='intersection', t_srs='first')
 
-    dem_ma_full = iolib.ds_getma(dem_ds)
+    src_ma_full = iolib.ds_getma(src_ds)
     mask_ma_full = iolib.ds_getma(mask_ds)
 
     if mask_ma_full.std() != 0:
-        #Input mask filename is actually a DEM, or other masked array
-        #Extract mask
+        #Input mask filename is a raster, or other masked array
+        #Just need to extract mask
         mask = np.ma.getmaskarray(mask_ma_full)
     else:
-        #Input mask filename is truly a mask, use directly
+        #Input mask filename is a mask, use directly
         #If input mask values are zero, valid values are nonzero
         #Bool True == 1, so need to invert
         if mask_ma_full.fill_value == 0:
@@ -42,14 +43,14 @@ def main():
 
     #Add dilation step for buffer
 
-    #newmask = np.logical_or(np.ma.getmaskarray(dem_ma_full), mask)
+    #newmask = np.logical_or(np.ma.getmaskarray(src_ma_full), mask)
 
     print("Creating new array with updated mask")
-    dem_ma_full = np.ma.array(dem_ma_full, mask=mask)
+    src_ma_full = np.ma.array(src_ma_full, mask=mask)
 
-    print("Writing out masked full-res DEM")
-    dem_fn_masked = os.path.splitext(dem_fn)[0]+'_masked.tif'
-    iolib.writeGTiff(dem_ma_full, dem_fn_masked, dem_ds, create=True, sparse=True)
+    src_fn_masked = os.path.splitext(src_fn)[0]+'_masked.tif'
+    print("Writing out masked version of input raster: %s" % src_fn_masked )
+    iolib.writeGTiff(src_ma_full, src_fn_masked, src_ds, create=True, sparse=True)
 
 if __name__ == '__main__':
     main()
