47182937712cd34b605e00999ff3f3f4f437ab88
David Shean <dshean@gmail.com>
2017-01-09 00:22:08 -0800

    Another try for read the docs build

--- docs/conf.py
+++ docs/conf.py
@@ -17,8 +17,19 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 import os
-#import sys
+import sys
 #sys.path.insert(0, os.path.abspath('..'))
+
+#from unittest.mock import MagicMock
+from mock import Mock as MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return MagicMock()
+
+MOCK_MODULES = ['gdal', 'numpy', 'scipy', 'matplotlib']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- General configuration ------------------------------------------------
 
