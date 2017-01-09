db50478456ab96a5bee4148edc509cdabf283e9e
David Shean <dshean@users.noreply.github.com>
2017-01-08 23:23:08 -0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -38,7 +38,7 @@ extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.autosummary',
-    'numpydoc'
+    'sphinxcontrib.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
