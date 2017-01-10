6b0343538d86ace49d837485734e8d926114bd74
David Shean <dshean@gmail.com>
2017-01-09 18:15:15 -0800

    warptool: update argparse help to include choices

--- pygeotools/warptool.py
+++ pygeotools/warptool.py
@@ -11,15 +11,26 @@ import argparse
 from pygeotools.lib import warplib
 from pygeotools.lib import geolib
 
+def fmt_choices(param, choices):
+    outstr = 'Valid choices for %s: {%s}\n' % (param, ','.join(choices))
+    return outstr
+
 def getparser():
-    #Can't specify arbitrary fn, res when limiting choices
+    #Note: Don't want to formally specify these choices, as it limits specification of arbitrary filenames
     tr_choices = ['first', 'last', 'min', 'max', 'mean', 'med', 'source', '"fn"', '"res"']
-    te_choices = ['first', 'last', 'intersection', 'union', 'source', '"fn"', '"extent"']
+    te_choices = ['first', 'last', 'intersection', 'union', 'source', '"fn"', '"xmin ymin xmax ymax"']
     t_srs_choices = ['first', 'last', '"fn"', '"proj4str"']
+    #Should read these directly from GDAL
     r_choices = ['near', 'bilinear', 'cubic', 'cubicspline', 'lanczos', 'average', 'mode']
-    
-    parser = argparse.ArgumentParser(description='Utility to warp stacks of rasters to the same res/extent/proj')
-    parser.add_argument('-tr', default='first', help='Output resolution (default: %(default)s)')
+
+    epilog = fmt_choices('-tr', tr_choices)
+    epilog += fmt_choices('-te', te_choices)
+    epilog += fmt_choices('-t_srs', t_srs_choices)
+
+    #Note, the formatter_class is a hack to show the choices in the epilog
+    parser = argparse.ArgumentParser(description='Utility to warp stacks of rasters to the same res/extent/proj', \
+            epilog=epilog, formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('-tr', default='first', help='Output resolution (default: %(default)s)') 
     parser.add_argument('-te', default='intersection', help='Output extent (default: %(default)s)')
     parser.add_argument('-t_srs', default='first', help='Output projection (default: %(default)s)')
     parser.add_argument('-dst_ndv', type=float, default=None, help='No data value for output')
