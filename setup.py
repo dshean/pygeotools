2fec6c11e784236c72e751b3945818c271795b1f
David Shean <dshean@gmail.com>
2017-06-11 12:36:01 -0700

    setup: update version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup
 #python setup.py sdist upload
 
 setup(name='pygeotools',
-    version='0.2.0',
+    version='0.3.0',
     description='Libraries and command-line utilities for geospatial data processing/analysis',
     author='David Shean',
     author_email='dshean@gmail.com',
