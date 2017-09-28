61fa5bbdbda8de8c3ee1a8b29d621a5ff9ff3dd5
David Shean <dshean@gmail.com>
2017-09-27 19:42:41 -0700

    timelib: tweak fn_getdatetime_list regexp

--- pygeotools/lib/timelib.py
+++ pygeotools/lib/timelib.py
@@ -104,15 +104,15 @@ def fn_getdatetime_list(fn):
     #Should probably search for all possibilities, then prune  
     #NOTE: these don't include seconds in the time
     dstr = None
-    dstr = re.findall(r'(?:^|_)(?:19|20)[0-9][0-9](?:0[1-9]|1[012])(?:0[1-9]|[12][0-9]|3[01])[_T](?:0[0-9]|1[0-9]|2[0-3])[0-5][0-9]', fn)
+    dstr = re.findall(r'(?:^|_|-)(?:19|20)[0-9][0-9](?:0[1-9]|1[012])(?:0[1-9]|[12][0-9]|3[01])[_T](?:0[0-9]|1[0-9]|2[0-3])[0-5][0-9]', fn)
     if not dstr:
-        dstr = re.findall(r'(?:^|_)(?:19|20)[0-9][0-9](?:0[1-9]|1[012])(?:0[1-9]|[12][0-9]|3[01])(?:0[0-9]|1[0-9]|2[0-3])[0-5][0-9]', fn)
+        dstr = re.findall(r'(?:^|_|-)(?:19|20)[0-9][0-9](?:0[1-9]|1[012])(?:0[1-9]|[12][0-9]|3[01])(?:0[0-9]|1[0-9]|2[0-3])[0-5][0-9]', fn)
     if not dstr:
-        dstr = re.findall(r'(?:^|_)(?:19|20)[0-9][0-9](?:0[1-9]|1[012])(?:0[1-9]|[12][0-9]|3[01])_', fn)
+        dstr = re.findall(r'(?:^|_|-)(?:19|20)[0-9][0-9](?:0[1-9]|1[012])(?:0[1-9]|[12][0-9]|3[01])(?:_|-)', fn)
         #This should pick up dates separated by a dash
         #dstr = re.findall(r'(?:^|_|-)(?:19|20)[0-9][0-9](?:0[1-9]|1[012])(?:0[1-9]|[12][0-9]|3[01])', fn)
     if not dstr:
-        dstr = re.findall(r'(?:^|_)(?:19|20)[0-9][0-9]_', fn)
+        dstr = re.findall(r'(?:^|_|-)(?:19|20)[0-9][0-9](?:_|-)', fn)
     #This is for USGS archive filenames
     if not dstr:
         dstr = re.findall(r'[0-3][0-9][a-z][a-z][a-z][0-9][0-9]', fn)
