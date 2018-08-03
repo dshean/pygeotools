9a6de4470cbd04aa62b10c7fc582c94ffec7ed09
David Shean <dshean@gmail.com>
2018-08-03 00:38:29 -0700

    raster2shp: parallel shp generation

--- pygeotools/raster2shp.py
+++ pygeotools/raster2shp.py
@@ -24,6 +24,15 @@ def getparser():
     parser.add_argument('fn_list', type=str, nargs='+', help='Input raster filename(s)')
     return parser
 
+def get_geom(fn):
+    shp_fn = None
+    if os.path.exists(fn):
+        shp_fn = os.path.splitext(fn)[0]+'.shp'
+        ds = gdal.Open(fn)
+        geom = geolib.get_outline(ds)
+        geolib.geom2shp(geom, shp_fn, fields=True)
+    return shp_fn
+
 def main():
     parser = getparser()
     args = parser.parse_args()
@@ -34,17 +43,16 @@ def main():
     #Note: ogr_merge.sh goes from first to last input shp
     #Reverse order here to preserve original order, with first input on top
     fn_list = fn_list[::-1]
+    print('%i input rasters' % len(fn_list))
+    print(fn_list[0])
 
-    merge_fn_list = []
-    for n,fn in enumerate(fn_list): 
-        print(n, fn)
-        if os.path.exists(fn):
-            shp_fn = os.path.splitext(fn)[0]+'.shp'
-            #if not os.path.exists(shp_fn):
-            ds = gdal.Open(fn)
-            geom = geolib.get_outline(ds)
-            geolib.geom2shp(geom, shp_fn, fields=True)
-            merge_fn_list.append(shp_fn)
+    import multiprocessing as mp
+    pool = mp.Pool()
+    results = pool.map(get_geom, fn_list)
+    merge_fn_list = sorted(results, reverse=True)
+
+    print('%i output shp' % len(merge_fn_list))
+    print(merge_fn_list[0])
 
     #This is a hack to merge, should just create new output and write all features
     if len(merge_fn_list) > 1:
