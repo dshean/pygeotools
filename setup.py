29c7ae85d2907bdbc3adaee389792cc7f88cabeb
Joachim Meyer <jomey@uw.edu>
2019-08-19 11:44:43 -0700

    Fix setup.py - Add requests library
    Commit 668ecc7 introduced a dependecy to the 'requests' library, but was
    not added to setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(name='pygeotools',
     url='https://github.com/dshean/pygeotools',
     packages=['pygeotools', 'pygeotools.lib'],
     long_description=open('README.md').read(),
-    install_requires=['gdal','numpy','scipy','matplotlib'],
+    install_requires=['gdal','numpy','scipy','matplotlib', 'requests'],
     #Note: this will write to /usr/local/bin
     #scripts=['pygeotools/warptool.py', 'pygeotools/make_stack.py', 'pygeotools/raster2shp.py', 'pygeotools/clip_raster_by_shp.py', 'pygeotools/apply_mask.py', 'pygeotools/mask_raster.sh', 'pygeotools/ogr_merge.sh', 'pygeotools/replace_ndv.py', 'pygeotools/trim_ndv.py', 'pygeotools/filter.py', 'pygeotools/proj_select.py']
 )
